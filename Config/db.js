277d15d73aac32e22f87ff8984bed551fb3c743b
Atharva Singh <atharva9arvin@gmail.com>
2024-11-19 23:45:33 +0530

    setting up to deploy

--- Config/db.js
+++ Config/db.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
 const connectDB = async() =>{
-    const conn = await mongoose.connect(process.env.DATABASE_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold)
+    try{
+        const conn = await mongoose.connect(process.env.DATABASE_URI)
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold)
+    }
+    catch(error){
+        console.error(`Error: ${error.message}`.red.underline.bold)
+        throw error
+    }
+   
 }
 
 module.exports = connectDB
